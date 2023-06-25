ec6a803705720d207b36e775aa272b8986cade02
kwanghwi <nimo6289@gmail.com>
2023-06-25 15:42:48 +0900

    init

---
+++ src/main/java/com/Han2m/portLogistics/PortLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.Han2m.portLogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortLogisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Han2m/portLogistics/PortLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Han2m.portLogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortLogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
