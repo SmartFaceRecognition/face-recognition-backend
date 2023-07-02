12be66bfd4a1dd4b4a505edd13d65ecf299bc276
kwanghwi <nimo6289@gmail.com>
2023-07-02 16:06:43 +0900

    IAM 해결되면 테스트 후 수정 예정

--- src/main/java/com/Han2m/portLogistics/controller/S3Controller.java
+++ src/main/java/com/Han2m/portLogistics/controller/S3Controller.java
@@ -5,40 +5,38 @@ import com.Han2m.portLogistics.entity.FileEntity;
 import com.Han2m.portLogistics.service.FileService;
 import com.Han2m.portLogistics.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class S3Controller {
 
     private final S3Service s3Service;
     private final FileService fileService;
 
-    @GetMapping("/api/upload")
-    public String goToUpload() {
-        return "upload";
+    @GetMapping("/api/files")
+    public ResponseEntity<List<FileEntity>> getAllFiles() {
+        List<FileEntity> fileList = fileService.getFiles();
+        return ResponseEntity.ok(fileList);
     }
 
-    @PostMapping("/api/upload")
-    public String uploadFile(FileDto fileDto) throws IOException {
-        String url = s3Service.uploadFile(fileDto.getFile());
+    @PostMapping("/api/files")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        String url = s3Service.uploadFile(file);
 
+        FileDto fileDto = new FileDto();
         fileDto.setUrl(url);
         fileService.save(fileDto);
 
-        return "redirect:/api/list";
-    }
-
-    @GetMapping("/api/list")
-    public String listPage(Model model) {
-        List<FileEntity> fileList =fileService.getFiles();
-        model.addAttribute("fileList", fileList);
-        return "list";
+        return ResponseEntity.ok(url);
     }
 }
