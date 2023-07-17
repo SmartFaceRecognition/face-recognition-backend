5e122690cf9af85366bd39d33d7b3ddb6e465c0d
kwanghwi <nimo6289@gmail.com>
2023-07-18 01:00:29 +0900

    .

--- src/main/java/com/Han2m/portLogistics/user/repository/PersonRepository.java
+++ src/main/java/com/Han2m/portLogistics/user/repository/PersonRepository.java
@@ -4,6 +4,10 @@ import com.Han2m.portLogistics.user.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> findByPosition(String position); // 직원 정보 조회
+    List<Person> findByPositionNot(String position); // 손님 정보 조회
 }
