9255c229176948f908554c48ef401606c9968496
mintaek <88085338+mintaek22@users.noreply.github.com>
2023-08-24 23:33:37 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,7 +3,7 @@
 ROOT_PATH="/home/ubuntu/face-backend"
 JAR="$ROOT_PATH/application.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
-SERVICE_PID=$(lsof -t -i :8080) # 실행중인 Spring 서버의 PID
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -n "$SERVICE_PID" ]; then
   echo "서비스 종료 " >> $STOP_LOG
