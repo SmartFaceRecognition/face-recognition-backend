8db6f82f2309e344fc257e279d328f92ca5b5771
mintaek22 <biba99@naver.com>
2023-09-13 11:25:33 +0900

    s3 파일 저장 url에 파일형식 추가:

--- src/main/java/com/Han2m/portLogistics/user/service/S3Service.java
+++ src/main/java/com/Han2m/portLogistics/user/service/S3Service.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @Service
@@ -39,13 +38,15 @@ public class S3Service {
             //content type을 지정해서 올려주지 않으면 자동으로 "application/octet-stream"으로 고정이 되서 링크 클릭시 웹에서 열리는게 아니라 자동 다운이 시작됨.
         };
 
+        String s3ObjectName = "faceImg/" + key.toString() + "." + ext;
+
         try {
             byte[] bytes = IOUtils.toByteArray(multipartFile.getInputStream());
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.setContentType(contentType);
             metadata.setContentLength(bytes.length);
 
-            amazonS3.putObject(new PutObjectRequest(bucket, "faceImg/"+key.toString(), multipartFile.getInputStream(), metadata)
+            amazonS3.putObject(new PutObjectRequest(bucket, s3ObjectName, multipartFile.getInputStream(), metadata)
                     .withCannedAcl(CannedAccessControlList.PublicRead));
         } catch (SdkClientException e) {
             e.printStackTrace();
